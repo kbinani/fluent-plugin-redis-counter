5fe6e6a02dba14d98c0c64bfc2d443f2803406f3
Keiji, Yoshimi <walf443@gmail.com>
2013-05-05 18:52:20 +0900

    added benchmark script.

---
+++ benchmark/performance_test.rb
@@ -0,0 +1,31 @@
+require 'fluent/test'
+require 'benchmark'
+
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")))
+require 'fluent/plugin/out_redis_counter'
+
+Fluent::Test.setup
+fluent = Fluent::Test::BufferedOutputTestDriver.new(Fluent::RedisCounterOutput).configure(%[
+    host localhost
+    port 6379
+    db_number 1
+    <pattern>
+      match_status ^2[0-9]{2}$
+      count_key_format status-normal:item_id:%_{item_id}
+    </pattern>
+])
+
+10_0000.times do
+  item_id = ( rand * 10_0000 ).to_i
+  fluent.emit({ "status" => 200, "item_id" => item_id })
+end
+
+result = nil
+Benchmark.bm do |x|
+  result = x.report {
+    fluent.run
+  }
+end
+
+$log.info("benchmark result: #{result}")
+
