4ffc989d917c9473f85c7fe6f09dbe79c5c5fe0f
Keiji, Yoshimi <walf443@gmail.com>
2013-05-05 19:39:26 +0900

    It does not work. match_key data should be String.

--- benchmark/performance_test.rb
+++ benchmark/performance_test.rb
@@ -12,12 +12,13 @@ fluent = Fluent::Test::BufferedOutputTestDriver.new(Fluent::RedisCounterOutput).
     <pattern>
       match_status ^2[0-9]{2}$
       count_key_format status-normal:item_id:%_{item_id}
+      count_value 1
     </pattern>
 ])
 
 10_0000.times do
   item_id = ( rand * 10_0000 ).to_i
-  fluent.emit({ "status" => 200, "item_id" => item_id })
+  fluent.emit({ "status" => "200", "item_id" => item_id })
 end
 
 result = nil
